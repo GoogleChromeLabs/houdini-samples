2d2860fcfbad01ec75fbf38cd8dbc2d80aa71891
Surma <surma@surma.link>
2016-01-24 14:14:19 +0000

    Add options box for parallax scrolling

---
+++ parallax-scrolling/options.js
@@ -0,0 +1,20 @@
+(function() {
+  var options = document.querySelectorAll('#options label');
+
+  // Init checkboxes
+  Array.prototype.forEach.call(options, function(option) {
+    var name = option.className.replace('.option', '').trim().replace('.', '');
+    if(!flagIsSet(name)) {
+      option.querySelector('input').checked = true;
+    }
+  });
+
+  document.querySelector('#options button').onclick = function() {
+    var opts = Array.prototype.filter.call(options, function(option) {
+      return !option.querySelector('input').checked;
+    }).map(function(option) {
+      return option.className.replace('.option', '').trim().replace('.', '');
+    });
+    location.search = '?' + opts.join('&');
+  };
+})();
