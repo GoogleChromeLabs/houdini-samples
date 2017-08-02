e82fe195a50961b3d25bc40359f28c56485c5a11
Majid Valipour <majidvp@chromium.org>
2017-08-02 12:05:46 -0400

    Make bar absolute pos and attach/detach timeline only once

--- animation-worklet/hidey-bar/hidey-bar-animator.js
+++ animation-worklet/hidey-bar/hidey-bar-animator.js
@@ -20,58 +20,78 @@ registerAnimator('hidey-bar', class TwitterHeader {
     this.lastBarPosition = 0;
     this.frameCount = 0;
   }
-  
+
   animate(currentTime, effect) {
     var scrollPosition = currentTime * this.options.scrollRange;
     // A primitive way to detect when scrolling has stopped. We really need ScrollTimeline.phase for
     // this
     var isScrolling = scrollPosition !== this.lastScrollPosition;
-    
+
     var barPosition = isScrolling ? this.scrollLinkedAnimation(scrollPosition)
                                   : this.timeLinkedAnimation(scrollPosition);
 
     effect.localTime = barPosition;
-    
+
+    // Attach to document timeline so we keep animating event when scrolling
+    // stops.
+    if (isScrolling)
+      this.attachDocumentTimeline();
+
     this.lastBarPosition = barPosition;
     this.lastScrollPosition = scrollPosition;
   }
-  
+
   scrollLinkedAnimation(scrollPosition) {
     var isUp = scrollPosition - this.lastScrollPosition <= 0;
     var position = this.lastBarPosition;
-    
+
     // We went from down -> up, reset the header position baseline.
     if (isUp && !this.wasUp)
       position = scrollPosition - this.options.barHeight;
-    
+
     if (isUp) {
       // scrolling up, move the header position with scroll so it appears fixed.
       position = Math.min(scrollPosition, position);
     } // else we are scrolling down, leave header position as is so it moves with content.
-    
+
     this.wasUp = isUp;
     return position;
   }
-  
+
   timeLinkedAnimation(scrollPosition) {
-    // attach document timeline so we keep ticking if scroll stops.
-    this.options.documentTimeline.attach(this);
-    
     // wait a few frames before starting the time linked animation.
     this.frameCount += 1;
     if (this.frameCount <= 10)
       return this.lastBarPosition;
-    
+
     var gap = scrollPosition - this.lastBarPosition;
     if (gap <= 0 || gap >= this.options.barHeight) {
       // bar is fully shown/hidden and no animation is needed so we detach timeline.
-      this.options.documentTimeline.detach(this);
-      this.frameCount = 0;
+      this.detachDocumentTimeline();
       return this.lastBarPosition;
     }
-    
-    // animate to fully hidden if the bar is more than half hidden or to fully shown otherwise. 
+
+    // animate to fully hidden if the bar is more than half hidden or to fully shown otherwise.
     var delta = gap <= (this.options.barHeight / 2) ? 2 : -2;
     return this.lastBarPosition + delta;
   }
+
+  attachDocumentTimeline() {
+    if (this.attachedTime)
+      return;
+
+    this.options.documentTimeline.attach(this);
+    this.attachedTime = true;
+    this.frameCount = 0;
+  }
+
+  detachDocumentTimeline() {
+    if (!this.attachedTime)
+      return;
+
+    this.options.documentTimeline.detach(this);
+    this.attachedTime = false;
+    this.frameCount = 0;
+  }
+
 });
