ed09cb156af9199e11f14e2b01fdaa2376dd33f0
Majid Valipour <majidvp@chromium.org>
2017-08-02 11:05:40 -0400

    Clean up

--- animation-worklet/hidey-bar/hidey-bar-animator.js
+++ animation-worklet/hidey-bar/hidey-bar-animator.js
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,61 +17,61 @@ registerAnimator('hidey-bar', class TwitterHeader {
   constructor(options) {
     this.options = options;
     this.lastScrollPosition = 0;
-    this.headerPosition = 0;
-    this.frames = 0;
+    this.lastBarPosition = 0;
+    this.frameCount = 0;
   }
-
+  
   animate(currentTime, effect) {
     var scrollPosition = currentTime * this.options.scrollRange;
+    // A primitive way to detect when scrolling has stopped. We really need ScrollTimeline.phase for
+    // this
     var isScrolling = scrollPosition !== this.lastScrollPosition;
     
-    if (isScrolling) {
-      this.scrollLinkedAnimation(scrollPosition);
-    } else {
-      this.timeLinkedAnimation(scrollPosition);
-    }
+    var barPosition = isScrolling ? this.scrollLinkedAnimation(scrollPosition)
+                                  : this.timeLinkedAnimation(scrollPosition);
 
-    effect.children[1].localTime = this.headerPosition;
+    effect.localTime = barPosition;
+    
+    this.lastBarPosition = barPosition;
     this.lastScrollPosition = scrollPosition;
   }
-
+  
   scrollLinkedAnimation(scrollPosition) {
     var isUp = scrollPosition - this.lastScrollPosition <= 0;
-
-    // if we went from down -> up, reset the header position baseline.
-    if (isUp && !this.wasUp) 
-      this.headerPosition = scrollPosition - this.options.barHeight;
-
+    var position = this.lastBarPosition;
+    
+    // We went from down -> up, reset the header position baseline.
+    if (isUp && !this.wasUp)
+      position = scrollPosition - this.options.barHeight;
+    
     if (isUp) {
       // scrolling up, move the header position with scroll so it appears fixed.
-      this.headerPosition = Math.min(scrollPosition, this.headerPosition);
-    } else {
-      // scrolling down, leave header position as is so it moves with content.
-    }
-
+      position = Math.min(scrollPosition, position);
+    } // else we are scrolling down, leave header position as is so it moves with content.
+    
     this.wasUp = isUp;
+    return position;
   }
-
+  
   timeLinkedAnimation(scrollPosition) {
-    // attach document timeline so we keep ticking if scroll stops
+    // attach document timeline so we keep ticking if scroll stops.
     this.options.documentTimeline.attach(this);
-
-    // wait a few frames before starting the timed animation
-    this.frames += 1;
-    if (this.frames <= 10)
-      return;
-
-    var gap = scrollPosition - this.headerPosition;
-    if (gap <= 0 || gap >=this.options.barHeight) {
-      // fully shown/hidden => no animation is needed, detach timeline
+    
+    // wait a few frames before starting the time linked animation.
+    this.frameCount += 1;
+    if (this.frameCount <= 10)
+      return this.lastBarPosition;
+    
+    var gap = scrollPosition - this.lastBarPosition;
+    if (gap <= 0 || gap >= this.options.barHeight) {
+      // bar is fully shown/hidden and no animation is needed so we detach timeline.
       this.options.documentTimeline.detach(this);
-      this.frames = 0;
-    } else if (gap >= (this.options.barHeight / 2)) {
-      // animate to fully hidden
-      this.headerPosition -= 2;
-    } else {
-      // animate to fully shown
-      this.headerPosition += 2;
+      this.frameCount = 0;
+      return this.lastBarPosition;
     }
+    
+    // animate to fully hidden if the bar is more than half hidden or to fully shown otherwise. 
+    var delta = gap <= (this.options.barHeight / 2) ? 2 : -2;
+    return this.lastBarPosition + delta;
   }
 });
