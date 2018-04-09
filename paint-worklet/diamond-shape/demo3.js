7ab51004d5044d52216205a0368d5836a1d1f5db
Surma <surma@surma.link>
2018-04-09 10:18:33 +0200

    Fix diamond for stable

--- paint-worklet/diamond-shape/demo3.js
+++ paint-worklet/diamond-shape/demo3.js
@@ -1,18 +1,18 @@
 registerPaint('demo', class {
-  static get inputArguments() { return ['<percentage>', '<percentage>']; }
-  
-  paint(ctx, geom, properties, args) {
-    
-    // ctx.filter = `blur(${Math.max(geom.width, geom.height) * 0.005}px)`;
+  static get inputProperties() { return ['--top-width', '--top-height']; }
+
+  paint(ctx, geom, properties) {
+    const topWidthPerc = parseInt(properties.get('--top-width').toString());
+    const topHeightPerc = parseInt(properties.get('--top-height').toString());
     ctx.beginPath();
     ctx.fillStyle = 'black';
-    const topWidth = geom.width * args[0].value/100;
+    const topWidth = geom.width * topWidthPerc/100;
     const left = (geom.width - topWidth)/ 2;
     ctx.moveTo(left, 0);
     ctx.lineTo(geom.width - left, 0);
-    ctx.lineTo(geom.width, geom.height * args[1].value/100);
+    ctx.lineTo(geom.width, geom.height * topHeightPerc/100);
     ctx.lineTo(geom.width / 2, geom.height);
-    ctx.lineTo(0, geom.height * args[1].value/100);
+    ctx.lineTo(0, geom.height * topHeightPerc/100);
     ctx.closePath();
     ctx.fill();
   }
