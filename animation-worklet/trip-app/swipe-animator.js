2028445c18f4e7f18204bdf92bf300bfa10114c1
Majid Valipour <majidvp@chromium.org>
2018-10-12 10:13:30 -0400

    initial  trip app implemnentation

---
+++ animation-worklet/trip-app/swipe-animator.js
@@ -0,0 +1,17 @@
+registerAnimator('material_tab_swipe', class {
+  constructor(options) {
+      console.log(options);
+      this.offset_ = options.offset;
+      this.width_ = options.width;
+  }
+  animate(currentTime, effect) {
+      const scrollOffset = currentTime;
+      var delta = Math.abs(scrollOffset - this.offset_);
+      // bug, at 1 we disable the animation!
+      const progress = Math.min(delta / this.width_, 0.999);
+      // if (progress > 0 && progress < 1)
+      //     console.log(`Article active at ${scrollOffset} with (${this.offset_}, ${this.width_}) => ${progress}`);
+
+      effect.localTime = progress * 100;
+  }
+})
