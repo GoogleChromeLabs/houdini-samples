99557576029d36c00e6f1cd1381f0b3f47b111f8
Majid Valipour <majidvp@chromium.org>
2018-10-16 13:52:53 -0400

    Add a swipe-to-action demo

--- animation-worklet/trip-app/swipe-animator.js
+++ animation-worklet/trip-app/swipe-animator.js
@@ -21,3 +21,37 @@ registerAnimator('passthrough', class {
         effect.localTime = currentTime;
     }
 });
+
+
+registerAnimator('icon_effect', class {
+  constructor(options) {
+      console.log(options);
+      this.start_ = options.start;
+      this.width_ = options.width;
+      this.play_when_favorited_ = options.play_when_favorited;
+      this.favorited_ = false;
+  }
+
+  animate(currentTime, effect) {
+      const scrollOffset = currentTime;
+      var delta = this.start_ - scrollOffset;
+      const progress = delta / this.width_;
+
+
+      if (progress < 0) {
+        return; // only animate in swipe right direction
+      } else if (progress > 0 && progress < 1) {
+        //console.log(`${progress}`);
+      } else if (progress >= 1) {
+        this.favorited_ = true;
+      }
+
+      if (!this.favorited_ && !this.play_when_favorited_) {
+        // bug, at 1 we disable the animation!
+        effect.localTime = Math.min(progress, 0.999) * 100;
+      } else if (this.favorited_ && this.play_when_favorited_) {
+        // play the transform animation
+        effect.localTime = Math.min(progress, 0.999) * 100;
+      }
+  }
+});
