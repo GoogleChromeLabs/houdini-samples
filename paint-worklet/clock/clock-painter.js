27d9bb01f452861c6671cad5f38ede491e741fdd
Surma <surma@surma.link>
2018-10-16 12:44:21 +0100

    First step of cleanup

--- paint-worklet/clock/clock-painter.js
+++ paint-worklet/clock/clock-painter.js
@@ -14,73 +14,79 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+function drawCircle(ctx, x, y, r, fill) {
+  ctx.fillStyle = fill;
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+
 class ClockPainter {
   static get inputProperties() {
     return ['--clock-time', '--clock-from-time'];
   }
 
+  constructor() {
+    this.clockHours = 12;
+  }
+
+  drawSwoosh(fromTime, toTime) {
+    const ctx = this.ctx;
+    const fromAngle = 2 * Math.PI * fromTime / this.clockHours - Math.PI / 2;
+    const toAngle = 2 * Math.PI * toTime / this.clockHours - Math.PI / 2;
+    const x1 = this.numbersRadius * Math.cos(fromAngle);
+    const y1 = this.numbersRadius * Math.sin(fromAngle);
+    const x2 = this.numbersRadius * Math.cos(toAngle);
+    const y2 = this.numbersRadius * Math.sin(toAngle);
+    const gradient = ctx.createLinearGradient(x1, y1, x2, y2);
+    gradient.addColorStop('0', 'rgba(0, 0, 255, 0)');
+    gradient.addColorStop('1', 'rgba(0, 0, 255, 1)');
+    ctx.strokeStyle = gradient;
+    ctx.lineWidth = 2 * this.dotRadius;
+    ctx.lineCap = 'round';
+    ctx.beginPath();
+    ctx.arc(0, 0, this.numbersRadius, fromAngle, toAngle);
+    ctx.stroke();
+  }
+
   paint(ctx, geom, properties) {
+    this.ctx = ctx;
     const xMid = geom.width / 2;
     const yMid = geom.height / 2;
-    const maxRadius = Math.min(xMid, yMid);
-    const clockHours = 12;
-    const dotRadius = 8;
-    const padding = 6;
-    const numbersRadius = maxRadius - dotRadius - padding;
+    this.maxRadius = Math.min(xMid, yMid);
+    this.dotRadius = 8;
+    this.padding = 6;
+    this.numbersRadius = this.maxRadius - this.dotRadius - this.padding;
 
     const clockTime = parseFloat(properties.get('--clock-time').toString());
     const clockFromTime =
         parseFloat(properties.get('--clock-from-time').toString());
 
-    const drawCircle = function(x, y, r, fill) {
-      ctx.fillStyle = fill;
-      ctx.beginPath();
-      ctx.arc(x, y, r, 0, 2 * Math.PI);
-      ctx.fill();
-    };
-
-    const drawArc = function(fromTime, toTime) {
-      const fromAngle = 2 * Math.PI * fromTime / clockHours - Math.PI / 2;
-      const toAngle = 2 * Math.PI * toTime / clockHours - Math.PI / 2;
-      const x1 = numbersRadius * Math.cos(fromAngle);
-      const y1 = numbersRadius * Math.sin(fromAngle);
-      const x2 = numbersRadius * Math.cos(toAngle);
-      const y2 = numbersRadius * Math.sin(toAngle);
-      const gradient = ctx.createLinearGradient(x1, y1, x2, y2);
-      gradient.addColorStop('0', 'rgba(0, 0, 255, 0)');
-      gradient.addColorStop('1', 'rgba(0, 0, 255, 1)');
-      ctx.strokeStyle = gradient;
-      ctx.lineWidth = 2 * dotRadius;
-      ctx.lineCap = 'round';
-      ctx.beginPath();
-      ctx.arc(0, 0, numbersRadius, fromAngle, toAngle);
-      ctx.stroke();
-    }
-
     ctx.translate(xMid, yMid);
-    drawCircle(0, 0, maxRadius, '#eee');
+    drawCircle(ctx, 0, 0, this.maxRadius, '#eee');
     // Draw the hour markers.
-    for (let hour = 1; hour <= clockHours; hour++) {
-      const angle = 2 * Math.PI * hour / clockHours - Math.PI / 2;
-      const x = numbersRadius * Math.cos(angle);
-      const y = numbersRadius * Math.sin(angle);
-      drawCircle(x, y, dotRadius, '#ccc');
+    for (let hour = 1; hour <= this.clockHours; hour++) {
+      const angle = 2 * Math.PI * hour / this.clockHours - Math.PI / 2;
+      const x = this.numbersRadius * Math.cos(angle);
+      const y = this.numbersRadius * Math.sin(angle);
+      drawCircle(ctx, x, y, this.dotRadius, '#ccc');
     }
     // Draw path trail.
-    drawArc(clockFromTime, clockTime);
+    this.drawSwoosh(clockFromTime, clockTime);
 
     // Draw the hour hand.
     ctx.save();
-    ctx.rotate(2 * Math.PI * clockTime / clockHours);
+    ctx.rotate(2 * Math.PI * clockTime / this.clockHours);
     ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
     ctx.beginPath();
     ctx.moveTo(-3, 0);
-    ctx.lineTo(0, -numbersRadius);
+    ctx.lineTo(0, -this.numbersRadius);
     ctx.lineTo(3, 0);
     ctx.lineTo(-3, 0);
     ctx.fill();
     ctx.restore();
-    drawCircle(0, 0, dotRadius, 'blue');
+    drawCircle(ctx, 0, 0, this.dotRadius, 'blue');
   }
 }
 
